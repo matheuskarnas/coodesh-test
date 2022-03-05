dd4cd84e9b1e9c413aefb519ff7d3e3606ea4adc
matheuskarnas <matheuskarnas1@gmail.com>
2022-03-05 07:18:09 -0300

    create GlobalStyle

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { ThemeProvider } from 'styled-components'
 import { App } from './App'
+import { GlobalStyle } from './globalStyle'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={{}}>
+      <App />
+      <GlobalStyle />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
