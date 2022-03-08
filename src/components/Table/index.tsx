cd933a04420268c1168269baca0e087dd70dba53
matheuskarnas <matheuskarnas1@gmail.com>
2022-03-08 09:18:28 -0300

    first fetch

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -1,5 +1,15 @@
+import axios, { AxiosResponse } from "axios"
+import { useState } from "react"
+
+
+
 export const Table = () => {
-    return(
+    const [data, setData] = useState({})
+    const response =  axios('https://randomuser.me/api/')
+        .then(r => setData(r.data.results))
+    console.log('data', data)
+
+    return (
         <h1>Table</h1>
     )
 }
