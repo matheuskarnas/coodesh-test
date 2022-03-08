b7a5db8ba8a0b2c896ce747dfdc4ae77457e5723
matheuskarnas <matheuskarnas1@gmail.com>
2022-03-08 09:44:00 -0300

    code review

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -1,15 +1,24 @@
 import axios, { AxiosResponse } from "axios"
 import { useState } from "react"
 
-
+const baseUrl = 'https://randomuser.me/api/'
 
 export const Table = () => {
-    const [data, setData] = useState({})
-    const response =  axios('https://randomuser.me/api/')
-        .then(r => setData(r.data.results))
-    console.log('data', data)
+    const [data, setData] = useState<object[]>([])
+
+    const axiosGet = () => {
+        axios.get(baseUrl)
+            .then(r => setData([...data, r.data.results[0]]))
+    }
+
+    axiosGet()
+    console.log('data',data)
+
 
     return (
+        <>
         <h1>Table</h1>
+        {data.map(p => <h1>{p.name.first}</h1>)}
+        </>
     )
 }
