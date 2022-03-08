93b93db77629fcf80fb3c4aa2de58dac15cc63d1
matheuskarnas <matheuskarnas1@gmail.com>
2022-03-08 14:04:04 -0300

    table is working

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -1,24 +1,54 @@
-import axios, { AxiosResponse } from "axios"
-import { useState } from "react"
+import axios from "axios"
+import { useEffect, useState } from "react"
+import { Grid } from "../Grid"
 
-const baseUrl = 'https://randomuser.me/api/'
+export type UserData = {
+    cell: string,
+    dob: {
+        age: number,
+        date: string,
+    },
+    email: string,
+    gender: string,
+    id: {
+        name: string,
+        value: string,
+    },
+    name: {
+        first: string,
+        last: string,
+        title: string,
+    }
+    nat: string,
+    picture: {
+        large: string,
+        medium: string,
+        thumbnail: string,
+    }
+}
+
+
+const baseUrl = 'https://randomuser.me/api/?results=50&exc=login,phone,registered'
 
 export const Table = () => {
-    const [data, setData] = useState<object[]>([])
+    const [data, setData] = useState<UserData[]>([])
 
-    const axiosGet = () => {
+    useEffect(() => {
         axios.get(baseUrl)
-            .then(r => setData([...data, r.data.results[0]]))
-    }
+            .then(r => setData(data => data.concat(r.data.results)))
+    }, [])
 
-    axiosGet()
-    console.log('data',data)
+
+
+    console.log('data', data)
 
 
     return (
         <>
-        <h1>Table</h1>
-        {data.map(p => <h1>{p.name.first}</h1>)}
+            <h1>Table</h1>
+            <h1>{data.length}</h1>
+            {/* {data.map(r => <h1>{r.id.name}</h1>)} */}
+            <Grid data={data} />
         </>
     )
 }
