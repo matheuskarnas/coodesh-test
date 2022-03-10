df4553cc95ef136dc4d2df5262a19def092abd6f
matheuskarnas <matheuskarnas1@gmail.com>
2022-03-10 13:48:23 -0300

    frist  adjust css on modal

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -8,11 +8,38 @@ const style = {
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
-    width: 400,
-    bgcolor: 'background.paper',
-    border: '2px solid #000',
+    minWidth: '300px',
     boxShadow: 24,
-    p: 4,
+    outline: 0,    
+
+    div: {
+        p: '0 1rem 1rem 1rem',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+
+        img: {
+            marginTop: '-64px ',
+            borderRadius: '50%',
+        },
+
+        h1: {
+            fontSize: '2rem',
+            display: 'flex',
+            alignItems: 'center',
+            flexDirection: 'column',
+            marginTop: '1rem',
+        },
+
+        h4: {
+            lineHeight: '1.5rem',
+            fontSize: '0.95rem',
+        },
+
+        h2: {
+            marginTop: '1rem',
+        },        
+    },
 };
 
 type ModalProps = {
@@ -26,26 +53,30 @@ export default function Modal({ open, handleClose, user }: ModalProps) {
 
 
     return (
-        <div>
-            <MUIModal
-                open={open}
-                onClose={handleClose}
-                aria-labelledby="modal-modal-title"
-                aria-describedby="modal-modal-description"
-            >
-                <Box sx={style}>
-                    <Typography id="modal-modal-title" variant="h1" component="h2">
-                        {user?.name.first}
-                    </Typography>
-                    <h2>nome: {user?.name.first}</h2>
-                    <h2>Email: {user?.email}</h2>
-                    <h2>genero: {user?.gender}</h2>
-                    <h2>nacimento: {user?.nat}</h2>
-                    <h2>Telefone: {user?.cell}</h2>
-                    <h2>Nacionalidade: {user?.nat}</h2>
+        <MUIModal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+        >
+            <Box sx={style}>
+                <Typography id="modal-modal-title" variant="h1" component="div">
+                    <img src={user?.picture.large} alt="Foto de perfil do usuario" />
+                    <h1>{`${user?.name.first} ${user?.name.last}`}</h1>
+                </Typography>
+                <Typography id="modal-modal-description" component="div" >
+                    <h4>nasc. 03/03/95</h4>
+                    <h4>Genero: {user?.gender}</h4>
+                    <h4>Nacio. {user?.nat}</h4>
+                    <h2>Contatos</h2>
+                    <p>
+                        <h4>Email: {user?.email}</h4>
+                        <h4>Telefone: {user?.cell}</h4>
+                        <h4>Endereço: Rua gelson carlos porto 222 vila bandeirante Caçapava-SP</h4>
+                    </p>
+                </Typography>
+            </Box>
+        </MUIModal>
 
-                </Box>
-            </MUIModal>
-        </div>
     );
 }
